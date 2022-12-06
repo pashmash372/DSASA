b44458f3f54988fb8deeab009e7c9b772669c69e
pashmash372 <abhilashsivasanker@gmail.com>
2022-12-06 19:24:21 +0530

    code updated

---
+++ Advance DSA/Day70-AdvancedDSATrees5-ProblemsonTrees/src/main/java/com/scaler/dsa/homework/PathSum.java
@@ -0,0 +1,105 @@
+package com.scaler.dsa.homework;
+
+import com.scaler.dsa.common.TreeNode;
+
+public class PathSum {
+    public int hasPathSum(TreeNode A, int B) {
+        boolean status = sum(A, 0, B);
+        return status ? 1 : 0;
+    }
+    public boolean sum(TreeNode A, int curSum, int reqSum) {
+        if (A == null) {
+            return false;
+        }
+        if (A.left == null && A.right == null) {
+            curSum += A.val;
+            if (curSum == reqSum)
+                return true;
+            return false;
+        }
+        int sum = curSum + A.val;
+        return sum(A.left, sum, reqSum) || sum(A.right, sum, reqSum);
+    }
+
+}
+
+/*Q2. Path Sum
+Solved
+character backgroundcharacter
+Stuck somewhere?
+Ask for help from a TA and get it resolved.
+Get help from TA.
+Problem Description
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+
+
+Problem Constraints
+1 <= number of nodes <= 105
+
+-100000 <= B, value of nodes <= 100000
+
+
+
+Input Format
+First argument is a root node of the binary tree, A.
+
+Second argument is an integer B denoting the sum.
+
+
+
+Output Format
+Return 1, if there exist root-to-leaf path such that adding up all the values along the path equals the given sum. Else, return 0.
+
+
+
+Example Input
+Input 1:
+
+ Tree:    5
+         / \
+        4   8
+       /   / \
+      11  13  4
+     /  \      \
+    7    2      1
+
+ B = 22
+Input 2:
+
+ Tree:    5
+         / \
+        4   8
+       /   / \
+     -11 -13  4
+
+ B = -1
+
+
+Example Output
+Output 1:
+
+ 1
+Output 2:
+
+ 0
+
+
+Example Explanation
+Explanation 1:
+
+ There exist a root-to-leaf path 5 -> 4 -> 11 -> 2 which has sum 22. So, return 1.
+Explanation 2:
+
+ There is no path which has sum -1.
+
+
+
+*/
+
+
+/*Recursion might make this problem much easier to solve.
+You just need to keep a track of the sum from the root to the current node.
+Then it becomes a question of just checking if the current node is a leaf node, and if so, do the sum match.
+
+*/
---
+++ Advance DSA/Day70-AdvancedDSATrees5-ProblemsonTrees/src/main/java/com/scaler/dsa/homework/Sumbinarytreeornot.java
@@ -0,0 +1,123 @@
+package com.scaler.dsa.homework;
+
+
+import com.scaler.dsa.common.TreeNode;
+
+public class Sumbinarytreeornot {
+    public static int isLeaf(TreeNode node) {
+        if (node == null) return 0;
+        if (node.left == null && node.right == null) return 1;
+        return 0;
+    }
+
+    public static int isSumTree(TreeNode node) {
+        int ls, rs;
+        if (node == null || isLeaf(node) == 1) return 1;
+
+        if (isSumTree(node.left) == 1 && isSumTree(node.right) == 1) {
+            if (node.left == null) ls = 0;
+            else if (isLeaf(node.left) == 1) ls = node.left.val;
+            else ls = 2 * (node.left.val);
+            if (node.right == null) rs = 0;
+            else if (isLeaf(node.right) == 1) rs = node.right.val;
+            else rs = 2 * (node.right.val);
+            if (node.val == ls + rs) return 1;
+            else return 0;
+        }
+        return 0;
+    }
+
+    public static int checksumtree(TreeNode root) {
+        if (isSumTree(root) == 1) return 1;
+        return 0;
+    }
+
+    public int solve(TreeNode A) {
+        return checksumtree(A);
+    }
+}
+
+
+/*Q3. Sum binary tree or not
+Solved
+character backgroundcharacter
+Stuck somewhere?
+Ask for help from a TA and get it resolved.
+Get help from TA.
+Problem Description
+Given a binary tree. Check whether the given tree is a Sum-binary Tree or not.
+
+Sum-binary Tree is a Binary Tree where the value of a every node is equal to sum of the nodes present in its left subtree and right subtree.
+
+An empty tree is Sum-binary Tree and sum of an empty tree can be considered as 0. A leaf node is also considered as SumTree.
+
+Return 1 if it sum-binary tree else return 0.
+
+
+
+Problem Constraints
+1 <= length of the array <= 100000
+
+0 <= node values <= 50
+
+
+
+Input Format
+The only argument given is the root node of tree A.
+
+
+
+Output Format
+Return 1 if it is sum-binary tree else return 0.
+
+
+
+Example Input
+Input 1:
+
+       26
+     /    \
+    10     3
+   /  \     \
+  4   6      3
+Input 2:
+
+       26
+     /    \
+    10     3
+   /  \     \
+  4   6      4
+
+
+Example Output
+Output 1:
+
+ 1
+Output 2:
+
+ 0
+
+
+Example Explanation
+Explanation 1:
+
+ All leaf nodes are considered as SumTree.
+ Value of Node 10 = 4 + 6.
+ Value of Node 3 = 0 + 3
+ Value of Node 26 = 20 + 6
+Explanation 2:
+
+ Sum of left subtree and right subtree is 27 which is not equal to the value of root node which is 26.
+
+
+
+*/
+
+/*A simple solution is to check for every node is their sub-tree sum is equal to value of that node. But it will take O(n2)
+
+An efficient approach is to store sum of subtree at the node, so we don’t need to calculate it again and again.
+
+1) If the node is a leaf node then sum of subtree rooted with this node is equal to value of this node.
+2) If the node is not a leaf node then sum of subtree rooted with this node is twice the value of this node (Assuming that the tree rooted with this node is SumTree).
+
+If all nodes are sumTree return 1 else return 0.*/
