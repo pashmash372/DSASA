bf5a7643230c13fc25477e0a0095a27a23982c13
pashmash372 <abhilashsivasanker@gmail.com>
2022-12-16 03:44:12 +0530

    code updated

--- Advance DSA/Day74-AdvancedDSAHeaps-2/src/test/java/com/scaler/dsa/assignment/AthlargestelementTest.java
+++ Advance DSA/Day74-AdvancedDSAHeaps-2/src/test/java/com/scaler/dsa/assignment/AthlargestelementTest.java
@@ -1,7 +1,29 @@
 package com.scaler.dsa.assignment;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class AthlargestelementTest {
 
+    @Test
+    public void solve() {
+
+        int[] B = {1, 2, 3, 4, 5, 6};
+        int A = 4;
+        Athlargestelement a = new Athlargestelement();
+        int[] ans = a.solve(A, B);
+        int[] expected = {-1, -1, -1, 1, 2, 3};
+        Assert.assertArrayEquals(expected, ans);
+    }
+
+    @Test
+    public void solve1() {
+
+        int[] B = {15, 20, 99, 1};
+        int A = 2;
+        Athlargestelement a = new Athlargestelement();
+        int[] ans = a.solve(A, B);
+        int[] expected = {-1, 15, 20, 20};
+        Assert.assertArrayEquals(expected, ans);
+    }
 }
--- Advance DSA/Day74-AdvancedDSAHeaps-2/src/test/java/com/scaler/dsa/assignment/KPlacesApartTest.java
+++ Advance DSA/Day74-AdvancedDSAHeaps-2/src/test/java/com/scaler/dsa/assignment/KPlacesApartTest.java
@@ -1,8 +1,27 @@
 package com.scaler.dsa.assignment;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class KPlacesApartTest {
 
+    @Test
+    public void solve() {
+        int[] A = {1, 40, 2, 3};
+        int B = 2;
+        KPlacesApart k = new KPlacesApart();
+        int[] ans = k.solve(A, B);
+        int[] expected = {1, 2, 3, 40};
+        Assert.assertArrayEquals(expected, ans);
+    }
 
+    @Test
+    public void solve1() {
+        int[] A = {2, 1, 17, 10, 21, 95};
+        int B = 1;
+        KPlacesApart k = new KPlacesApart();
+        int[] ans = k.solve(A, B);
+        int[] expected = {1, 2, 10, 17, 21, 95};
+        Assert.assertArrayEquals(expected, ans);
+    }
 }
