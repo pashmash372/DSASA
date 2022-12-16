8c09b47d6d6b69d412e745d361cc8ad0da336db9
pashmash372 <abhilashsivasanker@gmail.com>
2022-12-17 02:32:53 +0530

    code updated

--- Advance DSA/Day76-AdvancedDSAGreedyAlgorithms-1/src/test/java/com/scaler/dsa/homework/SeatsTest.java
+++ Advance DSA/Day76-AdvancedDSAGreedyAlgorithms-1/src/test/java/com/scaler/dsa/homework/SeatsTest.java
@@ -1,7 +1,25 @@
 package com.scaler.dsa.homework;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class SeatsTest {
 
+    @Test
+    public void solve() {
+        String A = "....x..xx...x..";
+        Seats s = new Seats();
+        int ans = s.seats(A);
+        int expected = 5;
+        Assert.assertEquals(expected, ans);
+    }
+
+    @Test
+    public void solve1() {
+        String A = "....xxx";
+        Seats s = new Seats();
+        int ans = s.seats(A);
+        int expected = 0;
+        Assert.assertEquals(expected, ans);
+    }
 }
