9fe61522ca9261d658d81296a9ef5697a2fa57b3
pashmash372 <abhilashsivasanker@gmail.com>
2022-12-27 17:37:31 +0530

    code updated

--- Advance DSA/Day84-AdvancedDSADynamicProgramming-4/src/main/java/com/scaler/dsa/assignment/Knapsack01.java
+++ Advance DSA/Day84-AdvancedDSADynamicProgramming-4/src/main/java/com/scaler/dsa/assignment/Knapsack01.java
@@ -6,23 +6,26 @@ import java.util.Arrays;
 public class Knapsack01 {
     static int maxn = 1009;
     static int[][] dp = new int[maxn][maxn];
+
     public static int knapsack(int[] A, int[] B, int C) {
-        for (int[] row: dp)
+        for (int[] row : dp)
             Arrays.fill(row, 0);
         int n = A.length;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= C; j++) {
                 dp[i][j] = dp[i - 1][j];
-                if (B[i - 1] <= j)
-                    dp[i][j] = Math.max(dp[i][j], A[i - 1] + dp[i - 1][j - B[i - 1]]);
+                if (B[i - 1] <= j) dp[i][j] = Math.max(dp[i][j], A[i - 1] + dp[i - 1][j - B[i - 1]]);
             }
         }
         return dp[n][C];
     }
+
     public int solve(int[] A, int[] B, int C) {
         return knapsack(A, B, C);
     }
 }
+
+
 /*Q2. 0-1 Knapsack
 Solved
 character backgroundcharacter
--- Advance DSA/Day84-AdvancedDSADynamicProgramming-4/src/test/java/com/scaler/dsa/assignment/Knapsack01Test.java
+++ Advance DSA/Day84-AdvancedDSADynamicProgramming-4/src/test/java/com/scaler/dsa/assignment/Knapsack01Test.java
@@ -1,7 +1,31 @@
 package com.scaler.dsa.assignment;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class Knapsack01Test {
+
+    @Test
+    public void solve() {
+        int[] A = {60, 100, 120};
+        int[] B = {10, 20, 30};
+        int C = 50;
+        Knapsack01 k = new Knapsack01();
+        int ans = k.solve(A, B, C);
+        int expected = 220;
+        Assert.assertEquals(expected, ans);
+    }
+
+    @Test
+    public void solve1() {
+        int[] A = {10, 20, 30, 40};
+        int[] B = {12, 13, 15, 19};
+        int C = 10;
+        Knapsack01 k = new Knapsack01();
+        int ans = k.solve(A, B, C);
+        int expected = 0;
+        Assert.assertEquals(expected, ans);
+    }
+
 
 }
