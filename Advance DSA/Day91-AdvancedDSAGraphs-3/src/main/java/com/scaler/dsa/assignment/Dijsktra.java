a993f7a9f325f3faa3b167b29385238d514a918e
pashmash372 <abhilashsivasanker@gmail.com>
2023-01-07 01:36:00 +0530

    code updated

--- Advance DSA/Day91-AdvancedDSAGraphs-3/src/main/java/com/scaler/dsa/assignment/Dijsktra.java
+++ Advance DSA/Day91-AdvancedDSAGraphs-3/src/main/java/com/scaler/dsa/assignment/Dijsktra.java
@@ -8,25 +8,18 @@ import java.util.PriorityQueue;
 public class Dijsktra {
     static int maxn = 100009;
     static int[] vis = new int[maxn];
-    static ArrayList <ArrayList< Pair >> adj;
+    static ArrayList<ArrayList<Pair>> adj;
+
     public static void graph() {
-        adj = new ArrayList < ArrayList < Pair > > (maxn);
+        adj = new ArrayList<ArrayList<Pair>>(maxn);
         for (int i = 0; i < maxn; i++) {
             vis[i] = 0;
-            adj.add(new ArrayList < Pair > ());
+            adj.add(new ArrayList<Pair>());
         }
-    }
-    public int[] solve(int A, int[][] B, int C) {
-        graph();
-        for (int[] edge: B) {
-            adj.get(edge[0]).add(new Pair(edge[2], edge[1]));
-            adj.get(edge[1]).add(new Pair(edge[2], edge[0]));
-        }
-        return dijkstra(A, C);
     }
 
     public static int[] dijkstra(int n, int source) {
-        PriorityQueue < Pair > pq = new PriorityQueue< Pair >(new CustomComp());
+        PriorityQueue<Pair> pq = new PriorityQueue<Pair>(new CustomComp()); // minheap
         int[] dist = new int[n];
         for (int i = 0; i < n; i++) {
             dist[i] = Integer.MAX_VALUE;
@@ -37,11 +30,10 @@ public class Dijsktra {
         while (pq.size() != 0) {
             Pair temp = pq.poll();
             int u = temp.b;
-            if (vis[u] == 1)
-                continue;
+            if (vis[u] == 1) continue;
             vis[u] = 1;
             for (int i = 0; i < adj.get(u).size(); i++) {
-                int v = adj.get(u).get(i).b, w = adj.get(u).get(i).a;
+                int v = adj.get(u).get(i).b, w = adj.get(u).get(i).a;   // (v,w)
                 if (dist[u] + w < dist[v]) {
                     dist[v] = dist[u] + w;
                     pq.offer(new Pair(dist[v], v));
@@ -49,20 +41,32 @@ public class Dijsktra {
             }
         }
         for (int i = 0; i < n; i++) {
-            if (dist[i] == Integer.MAX_VALUE)
-                dist[i] = -1;
+            if (dist[i] == Integer.MAX_VALUE) dist[i] = -1;
         }
         return dist;
     }
+
+    public int[] solve(int A, int[][] B, int C) {
+        graph();
+        for (int[] edge : B) {
+            adj.get(edge[0]).add(new Pair(edge[2], edge[1])); // a -> weight , b
+            adj.get(edge[1]).add(new Pair(edge[2], edge[0])); // b -> weight , a
+        }
+        return dijkstra(A, C);
+    }
 }
+
 class Pair {
     int a, b;
+
     public Pair(int u, int v) {
         this.a = u;
         this.b = v;
     }
 }
-class CustomComp implements Comparator< Pair > {
+
+// Increasing order
+class CustomComp implements Comparator<Pair> {
     @Override
     public int compare(Pair aa, Pair bb) {
         return aa.a - bb.a;
