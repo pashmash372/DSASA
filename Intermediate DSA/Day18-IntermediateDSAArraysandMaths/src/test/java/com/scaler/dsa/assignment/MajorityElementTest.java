ee0adb543363e44b395945e26e084ceedeea2d3e
pashmash372 <abhilashsivasanker@gmail.com>
2023-01-22 17:35:15 +0530

    code updated

--- Intermediate DSA/Day18-IntermediateDSAArraysandMaths/src/test/java/com/scaler/dsa/assignment/MajorityElementTest.java
+++ Intermediate DSA/Day18-IntermediateDSAArraysandMaths/src/test/java/com/scaler/dsa/assignment/MajorityElementTest.java
@@ -1,7 +1,19 @@
 package com.scaler.dsa.assignment;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class MajorityElementTest extends TestCase {
+import java.util.List;
+
+public class MajorityElementTest {
+
+    @Test
+    public void solve() {
+        MajorityElement m = new MajorityElement();
+        List<Integer> A = List.of(4, 3, 6, 3, 1, 3, 2, 5, 3, 3, 3);
+        int ans = m.majorityElement(A);
+        int expected = 3;
+        Assert.assertEquals(expected, ans);
+    }
 
 }
