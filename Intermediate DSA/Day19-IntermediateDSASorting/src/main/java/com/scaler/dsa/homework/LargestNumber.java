9e278544b01d04f22423bc43870f0ebae4101fa3
pashmash372 <abhilashsivasanker@gmail.com>
2023-01-23 18:33:57 +0530

    code updated

--- Intermediate DSA/Day19-IntermediateDSASorting/src/main/java/com/scaler/dsa/homework/LargestNumber.java
+++ Intermediate DSA/Day19-IntermediateDSASorting/src/main/java/com/scaler/dsa/homework/LargestNumber.java
@@ -6,34 +6,36 @@ import java.util.List;
 
 public class LargestNumber {
     // DO NOT MODIFY THE LIST
-    public String largestNumber(final List< Integer > A) {
+    public String largestNumber(final List<Integer> A) {
         StringBuffer strBuf = new StringBuffer();
-        Node num[];
+        Node[] num;
         int i = 0;
         num = new Node[A.size()];
-        for (int n: A) {
+        for (int n : A) {
             num[i] = new Node(n);
             i++;
         }
         // sorts the array lexicographically
         Arrays.sort(num);
-        for (Node n: num) {
-            if (n.number == 0 && strBuf.length() != 0)
-                continue;
+        for (Node n : num) {
+            if (n.number == 0 && strBuf.length() != 0) continue;
             strBuf.append(n.number);
         }
         return strBuf.toString();
     }
-    class Node implements Comparable < Node > {
+
+    class Node implements Comparable<Node> {
         int number;
+
         public Node(int number) {
             this.number = number;
         }
+
         @Override
         public int compareTo(Node o) {
-            String first = String.valueOf(this.number) + String.valueOf(o.number);
-            String second = String.valueOf(o.number) + String.valueOf(this.number);
-            return second.compareTo(first);
+            String first = String.valueOf(this.number) + o.number;
+            String second = String.valueOf(o.number) + this.number;
+            return second.compareTo(first); // sorts in decreasing order
         }
     }
 }
