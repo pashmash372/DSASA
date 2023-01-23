4c0f3cbffea5bb2d07479fba095fac0b3a7ab6d9
pashmash372 <abhilashsivasanker@gmail.com>
2023-01-23 19:20:11 +0530

    code updated

--- Intermediate DSA/Day19-IntermediateDSASorting/src/test/java/com/scaler/dsa/homework/LargestNumberTest.java
+++ Intermediate DSA/Day19-IntermediateDSASorting/src/test/java/com/scaler/dsa/homework/LargestNumberTest.java
@@ -1,8 +1,34 @@
 package com.scaler.dsa.homework;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class LargestNumberTest extends TestCase
-{
+import java.util.List;
 
+public class LargestNumberTest {
+
+    @Test
+    public void solve() {
+        LargestNumber l = new LargestNumber();
+        List<Integer> A = List.of(3, 30, 34, 5, 9);
+        String ans = l.largestNumber(A);
+        String expected = "9534330";
+        Assert.assertEquals(expected, ans);
+    }
+
+    @Test
+    public void solve1() {
+        LargestNumber l = new LargestNumber();
+        List<Integer> A = List.of(2, 3, 9, 0);
+        String ans = l.largestNumber(A);
+        String expected = "9320";
+        Assert.assertEquals(expected, ans);
+    } @Test
+    public void solve2() {
+        LargestNumber l = new LargestNumber();
+        List<Integer> A = List.of(0, 0, 0, 0, 0 );
+        String ans = l.largestNumber(A);
+        String expected = "0";
+        Assert.assertEquals(expected, ans);
+    }
 }
