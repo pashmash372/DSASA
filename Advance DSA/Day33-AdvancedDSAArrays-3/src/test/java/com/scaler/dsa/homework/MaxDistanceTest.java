61b26467065d3307e4d5fb43ac456c3db147405a
pashmash372 <abhilashsivasanker@gmail.com>
2023-02-10 13:54:42 +0530

    code updated

--- Advance DSA/Day33-AdvancedDSAArrays-3/src/test/java/com/scaler/dsa/homework/MaxDistanceTest.java
+++ Advance DSA/Day33-AdvancedDSAArrays-3/src/test/java/com/scaler/dsa/homework/MaxDistanceTest.java
@@ -1,7 +1,20 @@
 package com.scaler.dsa.homework;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MaxDistanceTest {
+
+    @Test
+    public void solve() {
+        int[] A = {3, 5, 4, 2};
+
+        MaxDistance m  = new MaxDistance();
+        int ans = m.maximumGap(A);
+        int expected=2;
+        assertEquals(expected,ans);
+    }
 
 }
