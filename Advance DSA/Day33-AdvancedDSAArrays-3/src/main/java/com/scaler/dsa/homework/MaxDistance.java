096e1d8d423a3b5f62faa71358a6e5bc0253de37
pashmash372 <abhilashsivasanker@gmail.com>
2023-02-10 13:45:36 +0530

    code updated

--- Advance DSA/Day33-AdvancedDSAArrays-3/src/main/java/com/scaler/dsa/homework/MaxDistance.java
+++ Advance DSA/Day33-AdvancedDSAArrays-3/src/main/java/com/scaler/dsa/homework/MaxDistance.java
@@ -1,21 +1,22 @@
 package com.scaler.dsa.homework;
 
 
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class MaxDistance {
     // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
     public int maximumGap(final int[] A) {
         int n = A.length;
-        int rmax[] = new int[n];
+        int[] rmax = new int[n];
         int i, j, max = -1;
         // rmax stores the maximum element in the suffix
-        for(i = n - 1; i >= 0; i--) {
-            rmax[i] = (i == n - 1 || A[i] > rmax[i + 1])? A[i] : rmax[i + 1];
+        for (i = n - 1; i >= 0; i--) {
+            rmax[i] = (i == n - 1 || A[i] > rmax[i + 1]) ? A[i] : rmax[i + 1];
         }
-        for(i = 0, j = 0; i < n && j < n; ){
-            if(A[i] <= rmax[j]){
-                if(j - i > max)
-                    max = j - i;
+        for (i = 0, j = 0; i < n && j < n; ) {
+            if (A[i] <= rmax[j]) {
+                if (j - i > max) max = j - i;
                 j++;
             } else {
                 i++;
@@ -24,6 +25,44 @@ public class MaxDistance {
         return max;
     }
 }
+
+/**/
+
+class MaxDistance1 {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int maximumGap(final int[] A) {
+
+        int n = A.length;
+        int max = Integer.MIN_VALUE;
+
+        //Create a 2D array to maintain the array elements and its indices
+        int[][] A_index = new int[A.length][2];
+
+        for (int i = 0; i < n; i++) {
+            A_index[i][0] = A[i];
+            A_index[i][1] = i;
+        }
+
+        //Sort the 2d array using comparator based on first elements
+        Arrays.sort(A_index, Comparator.comparingInt(a -> a[0]));
+
+    /*So afer sorting the max elements will be in the last So we are traversing from the last by tracking the index of max elements
+and subtracting it from the current lesser element and return the max value in the last */
+
+        int max_index = -1, ans = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int cur_index = A_index[i][1];
+            max_index = Math.max(cur_index, max_index);
+            ans = Math.max(ans, max_index - cur_index);
+        }
+
+        return ans;
+
+
+    }
+}
+
 
 /*Q1. Max Distance
 Solved
