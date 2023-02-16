5976592c1df159e224a24d2386969b3bfd817c06
pashmash372 <abhilashsivasanker@gmail.com>
2023-02-16 23:36:46 +0530

    code updated

--- Advance DSA/Day48-AdvancedDSABinarySearch-1/src/main/java/com/scaler/dsa/homework/SearchinBitonicArray.java
+++ Advance DSA/Day48-AdvancedDSABinarySearch-1/src/main/java/com/scaler/dsa/homework/SearchinBitonicArray.java
@@ -1,5 +1,69 @@
 package com.scaler.dsa.homework;
 
+/* This question is simlar to the rotated sorted array.
+ we will solve this in 3log(N) approach
+ 1st bin Search to find the pivot element -> log(n).
+ 2nd bin Search to find the element from 0 -> Pivot -> log(n). [Array is in increasing order]
+ 3rd bin Search to find the element from Pivot+1 -> N-1 -> log(n). [Array is in decreasing order]
+ Total time complexity is O(logN) <- O(3logN) <- O(logN+logN+logN).
+ However, since constraints is till 10^8, even a simple linear Search TC O(N) will alsso accepted for this question.
+*/
+
+class SearchinBitonicArray1 {
+    public int solve(int[] A, int B) {
+
+        int N = A.length;
+        int start = 1, end = N - 2;
+        // find the bitonic point.
+        int pivot = 0;
+        if (A[N - 1] > A[N - 2]) pivot = N - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) {
+                pivot = mid;
+                break;
+            } else if (A[mid] < A[mid - 1])// pivot is on the left
+                end = mid - 1;
+            else start = mid + 1;
+        }
+
+        // now that we have got the pivot we just have to do 2 binary searches 1 from 0->pivot && other from pivot+1 -> N-1.
+
+        start = 0;
+        end = pivot;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (A[mid] == B) return mid;
+            else if (A[mid] < B) start = mid + 1;
+            else end = mid - 1;
+        }
+
+        start = pivot + 1;
+        end = N - 1;
+        // elements from pivot to n-1 would be in descending order so adjust the conditions accordingly.
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (A[mid] == B) return mid;
+            else if (A[mid] < B) end = mid - 1;
+            else start = mid + 1;
+        }
+
+        return -1; // Element is not present in the array
+    }
+}
+
+// /* Linear Search O(N) approach
+// public class Solution {
+//     public int solve(int[] A, int B) {
+//        int N = A.length;
+//         for (int i=0; i< N; i++){
+//             if (A[i] == B)
+//                 return i;
+//         }
+//         return -1;
+//     }
+// }*/
+
 public class SearchinBitonicArray {
     /*// Function for binary search in ascending part
 int ascendingBinarySearch(vector < int > & arr, int low,
