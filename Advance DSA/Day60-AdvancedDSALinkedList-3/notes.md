e3b3908f7770abbc96adba37153ad433da41b252
pashmash372 <abhilashsivasanker@gmail.com>
2023-03-02 07:31:29 +0530

    code updated

--- Advance DSA/Day60-AdvancedDSALinkedList-3/notes.md
+++ Advance DSA/Day60-AdvancedDSALinkedList-3/notes.md
@@ -9,14 +9,14 @@
 
 # Homework
 
-| No.   | Name                     | Type                                                        | Solution link                                                               |
-|-------|--------------------------|-------------------------------------------------------------|-----------------------------------------------------------------------------|
-| - Q1. | Palindrome List          | Medium [![](https://img.shields.io/badge/-MEDIUM-yellow)]() | [SOLUTION](src/main/java/com/scaler/dsa/homework/PalindromeList.java)       |
-| - Q2. | Add Two Numbers as Lists | Medium [![](https://img.shields.io/badge/-MEDIUM-yellow)]() | [SOLUTION](src/main/java/com/scaler/dsa/homework/AddTwoNumbersasLists.java) |
-| - Q3. | Flatten a linked list    | Medium [![](https://img.shields.io/badge/-MEDIUM-yellow)]() | [SOLUTION](src/main/java/com/scaler/dsa/homework/Flattenalinkedlist.java)   |
-| - Q4. | Swap List Nodes in pairs | Easy [![](https://img.shields.io/badge/-EASY-green)]()      | [SOLUTION](src/main/java/com/scaler/dsa/homework/SwapListNodesinpairs.java) |
-| - Q5. | Clone a Linked List      | Medium [![](https://img.shields.io/badge/-MEDIUM-yellow)]() | [SOLUTION](src/main/java/com/scaler/dsa/homework/CloneaLinkedList.java)     |
-| - Q6. | Merge K Sorted Lists     | Hard  [![](https://img.shields.io/badge/-HARD-red)]()       | [SOLUTION](src/main/java/com/scaler/dsa/homework/MergeKSortedLists.java)    |
+| No.   | Name                     | Type                                                        | Solution link                                                                    |
+|-------|--------------------------|-------------------------------------------------------------|----------------------------------------------------------------------------------|
+| - Q1. | Palindrome List          | Medium [![](https://img.shields.io/badge/-MEDIUM-yellow)]() | [SOLUTION](src/main/java/com/scaler/dsa/homework/PalindromeList.java)            |
+| - Q2. | Add Two Numbers as Lists | Medium [![](https://img.shields.io/badge/-MEDIUM-yellow)]() | [SOLUTION](src/main/java/com/scaler/dsa/homework/AddTwoNumbersasLists.java)      |
+| - Q3. | Flatten a linked list    | Medium [![](https://img.shields.io/badge/-MEDIUM-yellow)]() | [SOLUTION](src/main/java/com/scaler/dsa/homework/Flattenalinkedlist.java)  tough |
+| - Q4. | Swap List Nodes in pairs | Easy [![](https://img.shields.io/badge/-EASY-green)]()      | [SOLUTION](src/main/java/com/scaler/dsa/homework/SwapListNodesinpairs.java)      |
+| - Q5. | Clone a Linked List      | Medium [![](https://img.shields.io/badge/-MEDIUM-yellow)]() | [SOLUTION](src/main/java/com/scaler/dsa/homework/CloneaLinkedList.java)          |
+| - Q6. | Merge K Sorted Lists     | Hard  [![](https://img.shields.io/badge/-HARD-red)]()       | [SOLUTION](src/main/java/com/scaler/dsa/homework/MergeKSortedLists.java)         |
 
 [![](https://img.shields.io/badge/github-blue?style=for-the-badge)](https://github.com/pashmash372)
 
--- Advance DSA/Day61-AdvancedDSAStacks-1/src/main/java/com/scaler/dsa/homework/MaximumFrequencystack.java
+++ Advance DSA/Day61-AdvancedDSAStacks-1/src/main/java/com/scaler/dsa/homework/MaximumFrequencystack.java
@@ -7,12 +7,13 @@ import java.util.Stack;
 
 public class MaximumFrequencystack {
     // to store frequence of an element in the stack.
-    private HashMap< Integer, Stack< Integer >> stacks = new HashMap < Integer, Stack < Integer >> ();
+    private final HashMap<Integer, Stack<Integer>> stacks = new HashMap<Integer, Stack<Integer>>();
     // to maintain the structure of stack and pop out top most element in case of a tie.
-    private HashMap < Integer, Integer > freq = new HashMap < Integer, Integer > ();
+    private final HashMap<Integer, Integer> freq = new HashMap<Integer, Integer>();
     private int maxfreq = 0;
-    public ArrayList< Integer > solve(ArrayList < ArrayList < Integer >> A) {
-        ArrayList < Integer > ans = new ArrayList < Integer > ();
+
+    public ArrayList<Integer> solve(ArrayList<ArrayList<Integer>> A) {
+        ArrayList<Integer> ans = new ArrayList<Integer>();
         for (int i = 0; i < A.size(); i++) {
             int oper = A.get(i).get(0);
             //push operation
@@ -26,17 +27,17 @@ public class MaximumFrequencystack {
                 int fr = freq.get(x);
                 maxfreq = Math.max(fr, maxfreq);
                 if (stacks.get(fr) != null) {
-                    Stack < Integer > tmp = stacks.get(fr);
+                    Stack<Integer> tmp = stacks.get(fr);
                     tmp.push(x);
                 } else {
-                    Stack < Integer > tmp = new Stack < Integer > ();
+                    Stack<Integer> tmp = new Stack<Integer>();
                     tmp.push(x);
                     stacks.put(fr, tmp);
                 }
                 ans.add(-1);
             } else {
                 //pop operation
-                Stack < Integer > tmp = stacks.get(maxfreq);
+                Stack<Integer> tmp = stacks.get(maxfreq);
                 int x = tmp.peek();
                 ans.add(x);
                 freq.put(x, maxfreq - 1);
@@ -47,6 +48,95 @@ public class MaximumFrequencystack {
         return ans;
     }
 }
+
+/* Java Solution (Approach Discussed) */
+
+/**
+ * Approach:
+ * Create two HashMaps & variable to store max frequency count.
+ * First one will contains Integer as key and Stack<Integer> as value
+ * Second HashMap will contain the frequency of each element;
+ * In the fist HashMap, the will keep all the elements in the stack with same frequency
+ * Let say you insert 15, 17 , 18 , 15.. the key 1 will have stack will contains 15, 17 &  18
+ * Key 2 will have 15 again, as it is the second occurrence of 15 & so on.
+ * So, whenever a element is added its frequency has to be updated in the second HashMap.
+ * The element has to be added in the first HashMap's stack according to its frequency.
+ * While POPING we have to do the reverse.
+ */
+
+
+class MaximumFrequencystack1 {
+
+
+    int maxCount = 0;
+
+    public ArrayList<Integer> solve(ArrayList<ArrayList<Integer>> A) {
+
+        HashMap<Integer, Stack> stackHm = new HashMap<>();
+        HashMap<Integer, Integer> freq = new HashMap<>();
+
+        int N = A.size();
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            int ops = A.get(i).get(0);
+            int val = A.get(i).get(1);
+
+            if (ops == 1) {
+                insert(ans, stackHm, freq, val);
+            } else {
+                pop(ans, stackHm, freq);
+            }
+        }
+
+        return ans;
+    }
+
+    public void insert(ArrayList<Integer> ans, HashMap<Integer, Stack> stackHm, HashMap<Integer, Integer> freq, int val) {
+        freq.put(val, freq.containsKey(val) ? freq.get(val) + 1 : 1);
+
+        int f = freq.get(val);
+
+        if (stackHm.containsKey(f)) {
+            Stack<Integer> st = stackHm.get(f);
+            st.push(val);
+            stackHm.put(f, st);
+        } else {
+            Stack<Integer> st = new Stack<Integer>();
+            st.push(val);
+            stackHm.put(f, st);
+            maxCount++;
+        }
+
+        ans.add(-1);
+
+    }
+
+    public void pop(ArrayList<Integer> ans, HashMap<Integer, Stack> stackHm, HashMap<Integer, Integer> freq) {
+
+        Stack<Integer> st = stackHm.get(maxCount);
+        int val = st.pop();
+        ans.add(val);
+
+        if (st.isEmpty()) {
+            stackHm.remove(maxCount);
+            maxCount--;
+        } else {
+            stackHm.put(maxCount, st);
+        }
+
+        int count = freq.get(val);
+        count--;
+
+        if (count == 0) {
+            freq.remove(val);
+        } else {
+            freq.put(val, count);
+        }
+
+    }
+}
+
 
 
 /*Q7. Maximum Frequency stack
