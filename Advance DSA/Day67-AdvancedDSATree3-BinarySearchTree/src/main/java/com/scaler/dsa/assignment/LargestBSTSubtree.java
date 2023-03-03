48b72314bebf5341cec15038a50b2fe1f6276c1c
pashmash372 <abhilashsivasanker@gmail.com>
2023-03-03 08:29:20 +0530

    code updated

--- Advance DSA/Day67-AdvancedDSATree3-BinarySearchTree/src/main/java/com/scaler/dsa/assignment/LargestBSTSubtree.java
+++ Advance DSA/Day67-AdvancedDSATree3-BinarySearchTree/src/main/java/com/scaler/dsa/assignment/LargestBSTSubtree.java
@@ -97,6 +97,56 @@ class Value {
 
 }
 
+/*Java simple solution | TC - O(N) | SC - O(N)*/
+
+class LargestBSTSubtree1 {
+    public int solve(TreeNode A) {
+        Pair pair = this.process(A);
+        return pair.size;
+    }
+
+    public Pair process(TreeNode node) {
+        if (node == null) {
+            return new Pair(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+
+        boolean valid = true;
+        Pair left = process(node.left);
+        Pair right = process(node.right);
+
+        if ((left.valid) && (right.valid)) {
+            if ((node.val <= left.max) || (node.val >= right.min)) {
+                valid = false;
+            }
+        } else {
+            valid = false;
+        }
+
+        int min = java.lang.Math.min(node.val, java.lang.Math.min(left.min, right.min));
+        int max = java.lang.Math.max(node.val, java.lang.Math.max(left.max, right.max));
+
+        if (valid) {
+            return new Pair(valid, left.size + right.size + 1, min, max);
+        }
+        return new Pair(valid, java.lang.Math.max(left.size, right.size), min, max);
+    }
+
+    class Pair {
+        boolean valid;
+        int min;
+        int max;
+        int size;
+
+        Pair(boolean valid, int size, int min, int max) {
+            this.valid = valid;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        }
+    }
+}
+
+
 
 /*Q2. Largest BST Subtree
 Solved
