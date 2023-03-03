63ce9f37520465b0860757d4e306097ea05e4aa6
pashmash372 <abhilashsivasanker@gmail.com>
2023-03-03 18:44:01 +0530

    code updated

--- Advance DSA/Day71-AdvancedDSATrees6-TriesofCharacters/src/main/java/com/scaler/dsa/assignment/AutoComplete.java
+++ Advance DSA/Day71-AdvancedDSATrees6-TriesofCharacters/src/main/java/com/scaler/dsa/assignment/AutoComplete.java
@@ -1,8 +1,6 @@
 package com.scaler.dsa.assignment;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 
 public class AutoComplete {
@@ -94,6 +92,7 @@ class TrieNode {
         isend = false;
     }
 }
+
 /*increasing  order */
 /* Pair class using Comparable interface so, use compareTo(a) method */
 /* in case of Comparator usage , use compare(a,b) method */
@@ -111,6 +110,116 @@ class pair implements Comparable<pair> {
         return this.first - a.first;
     }
 }
+
+/*Another Solution*/
+/*Java solution using Tries and TreeMap*/
+
+class Tries {
+    private final HashMap<Character, Tries> m_next;
+    private final SortedMap<Integer, Integer> m_weight;
+
+    public Tries() {
+        m_next = new HashMap<Character, Tries>();
+        m_weight = new TreeMap<Integer, Integer>(Collections.reverseOrder());
+    }
+
+    public boolean hasNext(Character data) {
+        return m_next.containsKey(data);
+    }
+
+    public Tries getNext(Character data) {
+        return m_next.get(data);
+    }
+
+    public Tries setNext(Character data) {
+        Tries temp = new Tries();
+        m_next.put(data, temp);
+        return temp;
+    }
+
+    public void autoComplete(String[] dictionary) {
+        int count = 1;
+        Set<Integer> keys = m_weight.keySet();
+        for (Integer key : keys) {
+            System.out.print(dictionary[m_weight.get(key)] + " ");
+
+            if (count < 5) count++;
+            else break;
+        }
+        System.out.println();
+    }
+
+    public void setWeight(int key, int value) {
+        m_weight.put(key, value);
+    }
+}
+
+class AutoComplete1 {
+    private static Tries insertIntoTries(Tries root, String[] dictionary, int[] weight) {
+        for (int i = 0; i < dictionary.length; i++) {
+            Tries temp = root;
+            String word = dictionary[i];
+            for (int j = 0; j < word.length(); j++) {
+                if (temp.hasNext(word.charAt(j))) temp = temp.getNext(word.charAt(j));
+                else temp = temp.setNext(word.charAt(j));
+
+                temp.setWeight(weight[i], i);
+            }
+        }
+
+        return root;
+    }
+
+    private static void searchOnTries(Tries root, String[] dictionary, String[] search) {
+        for (int i = 0; i < search.length; i++) {
+            Tries temp = root;
+            boolean bIsFound = true;
+            String prefix = search[i];
+            for (int j = 0; j < prefix.length(); j++) {
+                if (!temp.hasNext(prefix.charAt(j))) {
+                    bIsFound = false;
+                    System.out.println("-1 ");
+                    break;
+                }
+                temp = temp.getNext(prefix.charAt(j));
+            }
+
+            if (bIsFound) temp.autoComplete(dictionary);
+        }
+    }
+
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+
+        Scanner sc = new Scanner(System.in);
+        int Q = sc.nextInt();
+
+        while (Q-- > 0) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+
+            String[] dictionary = new String[N];
+            for (int i = 0; i < N; i++)
+                dictionary[i] = sc.next();
+
+            int[] weight = new int[N];
+            for (int j = 0; j < N; j++)
+                weight[j] = sc.nextInt();
+
+            String[] search = new String[M];
+            for (int k = 0; k < M; k++)
+                search[k] = sc.next();
+
+            Tries root = new Tries();
+            root = insertIntoTries(root, dictionary, weight);
+            searchOnTries(root, dictionary, search);
+        }
+    }
+}
+
 
 /*Q2. Auto Complete
 Solved
